45a277b5d45304575399d60c6567cc3b5c1a8b51
taka12natu <hvhpdrmcmtr12@gmail.com>
2021-09-11 19:28:35 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+for (let count = 1; count <= 100; count++){
+    if(count % 15 == 0 ){
+        console.log(`処理回数：${count}回目　アホ犬`);
+    }else if(count % 3 == 0 ){
+        console.log(`処理回数：${count}回目　アホ`);
+    }else if(count % 5 == 0 ){
+        console.log(`処理回数：${count}回目　犬`);
+    }
+}
+
+
+
+//チャレンジ課題
+for (let count = 1; count <= 100; count++){
+    if(count % 15 == 0 ){
+        console.log(`処理回数：${count}回目　アホ犬`);
+    }else if(count % 3 == 0 ){
+        console.log(`処理回数：${count}回目　アホ`);
+    }else if(count % 5 == 0 ){
+        console.log(`処理回数：${count}回目　犬`);
+    }else if(count % 2 == 0){
+        console.log(`処理回数：${count}回目`);
+    }
+}
